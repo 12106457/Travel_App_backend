b4de29a7799cde283051b8acc83f55933fd62bbf
12106457 <122795214+12106457@users.noreply.github.com>
2025-01-22 12:02:26 +0530

    Update masterdataController.js for correct file path

--- controller/masterdataController.js
+++ controller/masterdataController.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const couponCategoryData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/couponCategory.json')));
-const discountTypeData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/discountType.json')));
-const typeOfBookingData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/typeOfBooking.json')));
+const couponCategoryData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/couponCategory.json')));
+const discountTypeData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/discountType.json')));
+const typeOfBookingData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/typeOfBooking.json')));
 
 exports.adminPanelMasterData = (req, res) => {
     try {
