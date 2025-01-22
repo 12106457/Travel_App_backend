c37e393a563f1c44039a51606380c9b9a5420a60
12106457 <122795214+12106457@users.noreply.github.com>
2025-01-22 12:48:50 +0530

    Update masterdataController.js

--- controller/masterdataController.js
+++ controller/masterdataController.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const couponCategoryData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/couponCategory.json')));
-const discountTypeData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/discountType.json')));
-const typeOfBookingData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'optionList/adminpanel/typeOfBooking.json')));
-
-exports.adminPanelMasterData = (req, res) => {
-    try {
-        res.status(200).send({
-            status: true,
-            message: "Fetch master data successfully",
-            data: {
-                couponCategory: couponCategoryData.categories,
-                discountType: discountTypeData.discountType,
-                typeOfBooking: typeOfBookingData.bookingType
-            }
-        });
-    } catch (error) {
-        res.status(500).send({
-            status: false,
-            message: "Error fetching master data",
-            error: error.message
-        });
-    }
-};
+const couponCategoryData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/couponCategory.json')));
+const discountTypeData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/discountType.json')));
+const typeOfBookingData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/typeOfBooking.json')));
+console.log("couponCategoryData:",couponCategoryData);
+console.log("discountTypeData:",discountTypeData);
+console.log("typeOfBookingData:",typeOfBookingData);
+// exports.adminPanelMasterData = (req, res) => {
+//     try {
+//         res.status(200).send({
+//             status: true,
+//             message: "Fetch master data successfully",
+//             data: {
+//                 couponCategory: couponCategoryData.categories,
+//                 discountType: discountTypeData.discountType,
+//                 typeOfBooking: typeOfBookingData.bookingType
+//             }
+//         });
+//     } catch (error) {
+//         res.status(500).send({
+//             status: false,
+//             message: "Error fetching master data",
+//             error: error.message
+//         });
+//     }
+// };
