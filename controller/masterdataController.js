3ac1dc3fced78f74f018e247e6bb5da61ce168f7
12106457 <122795214+12106457@users.noreply.github.com>
2025-01-22 11:56:47 +0530

    Update masterdataController.js

--- controller/masterdataController.js
+++ controller/masterdataController.js
@@ -1,24 +1,26 @@
-const mongoose = require("mongoose");
-const couponCategoryData = require("../optionList/adminpanel/couponCategory.json");
-const discountTypeData = require("../optionList/adminpanel/discountType.json");
-const typeOfBookingData = require("../optionList/adminpanel/typeOfBooking.json");
+const fs = require('fs');
+const path = require('path');
+
+const couponCategoryData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/couponCategory.json')));
+const discountTypeData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/discountType.json')));
+const typeOfBookingData = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../optionList/adminpanel/typeOfBooking.json')));
 
 exports.adminPanelMasterData = (req, res) => {
-  try {
-    res.status(200).send({
-      status: true,
-      message: "Fetch master data successfully",
-      data: {
-        couponCategory: couponCategoryData.categories,
-        discountType: discountTypeData.discountType,
-        typeOfBooking: typeOfBookingData.bookingType,
-      },
-    });
-  } catch (error) {
-    res.status(500).send({
-      status: false,
-      message: "Error fetching master data",
-      error: error.message,
-    });
-  }
+    try {
+        res.status(200).send({
+            status: true,
+            message: "Fetch master data successfully",
+            data: {
+                couponCategory: couponCategoryData.categories,
+                discountType: discountTypeData.discountType,
+                typeOfBooking: typeOfBookingData.bookingType
+            }
+        });
+    } catch (error) {
+        res.status(500).send({
+            status: false,
+            message: "Error fetching master data",
+            error: error.message
+        });
+    }
 };
